e8d4916f2cefcbd3cf090602f9eb0600055e84d6
Joshua Tucker <thejltucker@gmail.com>
2019-10-25 14:44:36 -0400

    Created ApplicationDbContext and DefaultConnection connection string with Book Table

---
+++ BookListRazor/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+    }
+}
