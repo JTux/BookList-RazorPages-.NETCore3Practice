1791cccf453816fb2b9d8716192bbeb50559fc83
Joshua Tucker <thejltucker@gmail.com>
2019-10-29 13:28:09 -0400

    Book Index and Create Functionality and Pages created

---
+++ BookListRazor/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        // Get handler
+        public async Task OnGet()
+        {
+            Books = await _context.Books.ToListAsync();
+        }
+    }
+}
