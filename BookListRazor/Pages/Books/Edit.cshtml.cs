151450ca203629807b0c0fb1fe6e15a28a8df734
Joshua Tucker <thejltucker@gmail.com>
2019-10-29 13:51:20 -0400

    Edit Page and PageModel created with OnGetAsync method

---
+++ BookListRazor/Pages/Books/Edit.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.Books
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _context;
+
+        public EditModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGetAsync(int id)
+        {
+            Book = await _context.Books.FindAsync(id);
+        }
+    }
+}
