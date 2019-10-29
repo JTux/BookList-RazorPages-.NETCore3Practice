a5133997833abb30e45ce83093586bf71bcbe69c
Joshua Tucker <thejltucker@gmail.com>
2019-10-29 14:24:07 -0400

    Cleaned up Index and added Delete functionality

--- BookListRazor/Pages/Books/Index.cshtml.cs
+++ BookListRazor/Pages/Books/Index.cshtml.cs
@@ -24,5 +24,15 @@ namespace BookListRazor.Pages.Books
         {
             Books = await _context.Books.ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null) return NotFound();
+            _context.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
     }
 }
